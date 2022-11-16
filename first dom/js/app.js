269ee7a667f8202c03c45652875de149a2e3738b
vovkaWeb <vova77557@gmail.com>
2022-11-16 20:11:07 +0200

    first work with dom

---
+++ first dom/js/app.js
@@ -0,0 +1,8 @@
+const div = document.querySelector('div');
+const titles = document.querySelectorAll('h1');
+// const h1 = document.getElementsByTagName('h1');
+// console.dir(h1);
+// console.log(titles);
+// console.log(Array.from(titles));
+// console.log(Array.prototype.slice.call(titles));
+// console.log([...titles]);
