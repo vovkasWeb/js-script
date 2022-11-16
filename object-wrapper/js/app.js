9eea65c3488b8bace0b74447d6db1cc6c7d3d7b2
vovkaWeb <vova77557@gmail.com>
2022-11-16 10:14:51 +0200

    yesterday's study

---
+++ object-wrapper/js/app.js
@@ -0,0 +1,7 @@
+let str = "Hello world";
+console.log(str.length);
+console.log(str.slice(1,-1));
+console.dir(String('hello').slice(1,-1));
+const strObj = new String('hello');
+console.log(strObj);
+console.dir(Number);
