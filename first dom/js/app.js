b2bf8df1f9f9583780fe172bf4a93abc2ba26488
vovkaWeb <vova77557@gmail.com>
2022-11-17 09:42:08 +0200

    updated file

--- first dom/js/app.js
+++ first dom/js/app.js
@@ -1,8 +1,25 @@
 const div = document.querySelector('div');
 const titles = document.querySelectorAll('h1');
-// const h1 = document.getElementsByTagName('h1');
-// console.dir(h1);
-// console.log(titles);
-// console.log(Array.from(titles));
+ let h1 = document.getElementsByTagName('h1');
+//  console.dir(h1);
+//  console.log(titles);
+ //console.log(Array.from(titles));
 // console.log(Array.prototype.slice.call(titles));
-// console.log([...titles]);
+//  console.log([...h1]);
+
+
+ console.log(document.querySelector('head')); 
+ console.log(document.querySelector('body'));
+ console.log(document.querySelector('body').children);
+ console.log([...document.querySelector('div').children]);
+ let dev = [...document.querySelector('div').children];
+
+
+ function isParent(parent, child) {
+	return child.parentNode===parent;
+}
+
+ console.log(isParent(document.body.children[0], document.querySelector('mark')));
+
+ const filterdLink = [...document.links].filter(a=>!a.closest('ul'));
+ console.log(filterdLink);
